803cb9504c4eb4bb1eb714a195546679afebc4fb
Daffa Damar <daffaardhika@gmail.com>
2023-05-29 09:44:19 +0700

    dev: Create posts page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const metadata = {
+  title: "Posts | Daffa Damar",
+  description: "Daffa Damar Posts's",
+};
+
+const page = () => {
+  return (
+    <div className="container flex flex-col py-12 w-full items-center h-[85vh] md:h-[90vh] xl:h-[89vh]">
+      <div className="flex flex-col items-center justify-between h-1/2">
+        <h1>Posts</h1>
+        <h2>Coming soon!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
