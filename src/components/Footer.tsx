59920b89a059a7e47baf8a585a75b07782b5d0a5
daffadmr <daffaardhika@gmail.com>
2024-01-28 20:24:48 +0700

    add dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const d = new Date();
   return (
     <footer className="border-t py-3">
       <div className="container">
         <div className="flex justify-center">
-          <p>copyright 	&copy; 2023 - daffadmr</p>
+          <p>copyright &copy; {d.getFullYear()} - daffadmr</p>
         </div>
       </div>
     </footer>
