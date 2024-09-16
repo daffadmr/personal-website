697fdcc1f950cb9904e9a7746656c78baf8365af
daffadmr <daffaardhika@gmail.com>
2024-09-16 19:10:10 +0700

    upgrade next version and react version and add react-use package

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DarkModeBtn from "./DarkModeBtn";
+import { useClickAway } from "react-use";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
+
+  const ref = useRef(null);
+
+  useClickAway(ref, () => {
+    setShowNav(false);
+  });
 
   const pages = [
     {
@@ -35,8 +42,37 @@ const Navbar = () => {
             <span className="hamburger-line origin-bottom-left transition duration-500 ease-in-out"></span>
           </button>
           <nav
-            className={`bg-white border md:border-0 dark:bg-slate-800  dark:border-white absolute right-5 top-16 max-w-[250px] w-[80vw] py-0 shadow-lg rounded-lg md:flex md:static md:bg-transparent md:shadow-none md:py-0 md:justify-end ${
+            className={`bg-white border dark:bg-slate-800  dark:border-white absolute right-5 top-16 max-w-[250px] w-[80vw] py-0 shadow-lg rounded-lg ${
               showNav ? "" : "hidden"
+            }`}
+            ref={ref}
+          >
+            <div className="flex flex-col-reverse md:flex-row gap-5 p-2 pb-5 md:p-0">
+              <ul className="flex flex-col md:flex-row gap-3 md:items-center">
+                {pages.map((page, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className="inline-block hover:bg-gray-300 dark:hover:bg-slate-600 px-[5px] rounded"
+                    >
+                      <Link
+                        className="block"
+                        href={page.link}
+                        onClick={() => setShowNav(false)}
+                      >
+                        {page.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="flex justify-end">
+                <DarkModeBtn />
+              </div>
+            </div>
+          </nav>
+          <nav
+            className={`bg-white border-0 dark:bg-slate-800  dark:border-white max-w-[250px] w-[80vw] rounded-lg md:flex md:static bg-transparent shadow-none py-0 justify-end hidden
             }`}
           >
             <div className="flex flex-col-reverse md:flex-row gap-5 p-2 pb-5 md:p-0">
