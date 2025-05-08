1c6200c83de3b0d6d9cfd659dec7ee228111b5bd
Daffa Damar <daffaardhika@gmail.com>
2025-05-08 22:06:50 +0700

    add github calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Contact from "@/components/Contact";
 import Image from "next/image";
 import React from "react";
+import GitHubCalendar from "react-github-calendar";
 
 const page = () => {
   return (
-    <main className="container py-12 lg:py-20 flex flex-col justify-center items-center md:min-h-[89vh]">
-      <div className="hero pb-6 md:pb-20 flex flex-col md:flex-row justify-center items-center gap-6 lg:gap-12">
+    <main className="container py-12 flex flex-col justify-center items-center md:min-h-[89vh]">
+      <div className="hero pb-6 md:pb-12 flex flex-col md:flex-row justify-center items-center gap-6 lg:gap-12">
         <Image
           src={"me.jpg"}
           unoptimized={true}
@@ -25,12 +26,15 @@ const page = () => {
           <Contact />
         </div>
       </div>
-      <p className="text-justify">
+      <p className="text-center">
         My name is Daffa Damar Ardhika, a frontend developer currently focused
         on Vue.js, with experience in React as well. I enjoy crafting seamless
         and interactive user experiences and am passionate about using Tailwind
         CSS for styling and layouting.
       </p>
+      <div className="py-12 gap-5 flex flex-col flex-wrap overflow-auto max-w-[80dvw]">
+        <GitHubCalendar username="daffadmr" />
+      </div>
     </main>
   );
 };
